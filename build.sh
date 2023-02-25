32eac202a5ccb7d163bd3016502a9696dd5f663e
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2023-02-25 11:45:56 +0000

    feat: update UFW X-UI port

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# sudo ufw allow 54321
 docker-compose --env-file .env up -d --build
--- scripts/setup-ufw.sh
+++ scripts/setup-ufw.sh
@@ -5,3 +5,4 @@ sudo ufw default allow outgoing
 ufw allow ssh
 ufw allow http
 ufw allow https
+sudo ufw allow 54321 # X-UI dashboard
